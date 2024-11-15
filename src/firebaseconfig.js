c9964bf6cba783381f79b81c3df0fb4f90d9d70b
Javier Rojas <jardlml1209@gmail.com>
2024-11-14 21:51:22 -0300

    update Firebase configuration to use environment variables and add .env to .gitignore

--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -7,19 +7,15 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDaQ53jLFsEINA4Bm0cxH4lClABcD6z3rQ",
-  authDomain: "crud-usuarios-ec763.firebaseapp.com",
-  projectId: "crud-usuarios-ec763",
-  storageBucket: "crud-usuarios-ec763.firebasestorage.app",
-  messagingSenderId: "927606262783",
-  appId: "1:927606262783:web:bade7f575bb69cb8aeb5ca"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const db = firebaseApp.firestore();
-// const db = getFirestore(firebaseApp);
-
-// export { db };
 
 export default app;
